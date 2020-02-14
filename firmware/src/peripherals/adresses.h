c80b72c2dc3e124517325b81890e7a66c6972b93
JeppeSRC <theholyhorse.dev@gmail.com>
2020-02-14 22:00:01 +0100

    added aaddresses and files

---
+++ firmware/src/peripherals/adresses.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#define GPIOF 0x48001400
+#define GPIOD 0x48000C00
+#define GPIOC 0x48000800
+#define GPIOB 0x48000400
+#define GPIOA 0x48000000
+
+#define RCC 0x40021000
+
+#define SYSCFG 0x40010000
+#define COMP SYSCFG
+
+#define DAC1 0x40007400
+#define DAC2 0x40009800
+
+#define ADC1 0x50000000
