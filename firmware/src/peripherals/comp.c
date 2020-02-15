971d81b4cba80267ccf46f421995460dd5bbf372
JeppeSRC <theholyhorse.dev@gmail.com>
2020-02-15 09:37:35 +0100

    Changed dac for comp4

--- firmware/src/peripherals/comp.c
+++ firmware/src/peripherals/comp.c
@@ -4,5 +4,6 @@ void InitializeComparators() {
 	volatile unsigned int* comp2 = (volatile unsigned int*)COMP + 0x20;
 	volatile unsigned int* comp4 = (volatile unsigned int*)COMP + 0x28;
 
-	*comp2 = *comp4 = 0x41;
+	*comp2 = 0x41; //Uses DAC1_CH1
+	*comp4 = 0x51; //Uses DAC1_CH2
 }
