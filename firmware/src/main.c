c94b59853d6573115f9658b98a9e1577ed0b3ad2
JeppeSRC <theholyhorse.dev@gmail.com>
2020-02-15 14:11:03 +0100

    Clock initialization

--- firmware/src/main.c
+++ firmware/src/main.c
@@ -1,7 +1,24 @@
 #include <peripherals/gpio.h>
+#include <peripherals/rcc.h>
 
 int main() {
+	rcc_cr_pllon(0);
 
+	while (rcc_cr_pllrdy());
+
+	rcc_cfgr_pllmul(15);
+	rcc_cfgr_pllsrc(0);
+	rcc_cfgr_ppre2(0);
+	rcc_cfgr_ppre1(4);
+	rcc_cr_pllon(1);
+
+	while (!rcc_cr_pllrdy());
+
+	rcc_cfgr_sw(2);
+
+	rcc_ahbenr();
+	rcc_apb2enr();
+	rcc_apb1enr();
 
 	return 0;
 }
