6641c27f3df1cbb2502208a852f5ebc4066e88aa
JeppeSRC <theholyhorse.dev@gmail.com>
2020-02-26 21:05:55 +0100

    Added comp_output function that reads the status of the comparator

--- firmware/src/peripherals/comp.c
+++ firmware/src/peripherals/comp.c
@@ -1,9 +1,17 @@
 #include "comp.h"
 
-void initialize_comparators() {
-	volatile unsigned int* comp2 = (volatile unsigned int*)COMP + 0x20;
-	volatile unsigned int* comp4 = (volatile unsigned int*)COMP + 0x28;
+volatile unsigned int* comp2 = (volatile unsigned int*)COMP + 0x20;
+volatile unsigned int* comp4 = (volatile unsigned int*)COMP + 0x28;
 
+void initialize_comparators() {
 	*comp2 = 0x41; //Uses DAC1_CH1
 	*comp4 = 0x51; //Uses DAC1_CH2
+}
+
+unsigned char comp2_output() {
+	return (*comp2 >> 30) & 0x01;
+}
+
+unsigned char comp4_output() {
+	return (*comp4 >> 30) & 0x01;
 }
--- firmware/src/peripherals/comp.h
+++ firmware/src/peripherals/comp.h
@@ -2,3 +2,6 @@
 #include <peripherals/addresses.h>
 
 void initialize_comparators();
+
+unsigned char comp2_output();
+unsigned char comp4_output();
